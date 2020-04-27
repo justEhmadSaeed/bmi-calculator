563e5afb89680cdaf99a35ca175092762c3b3ccf
Ehmad <justEhmadSaeed@gmail.com>
2020-04-28 01:23:42 +0500

    👌 IMPROVE: Refactor container to ResuseableCard class

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,17 +9,56 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Container(
-        height: 200,
-        width: 170,
-        margin: EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: Color(0xFF1D1E33),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
         ),
+        body: Column(children: [
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: ReuseableCard(),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ReuseableCard(),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReuseableCard(),
+          ),
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: ReuseableCard(),
+              ),
+              Expanded(
+                flex: 1,
+                child: ReuseableCard(),
+              )
+            ],
+          )),
+        ]));
+  }
+}
+
+class ReuseableCard extends StatelessWidget {
+  const ReuseableCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Color(0xFF1D1E33),
       ),
     );
   }
