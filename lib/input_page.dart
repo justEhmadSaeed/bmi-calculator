6a07fd0aca135ab29d7bb36804dacdfad083b4df
Ehmad <justEhmadSaeed@gmail.com>
2020-04-28 16:49:29 +0500

    📦 NEW: Add font awesome package

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const cardColor = Color(0xFF1D1E33);
 const bottomContainerHeight = 80.0;
@@ -58,11 +59,15 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReuseableCard extends StatelessWidget {
+  ReuseableCard({@required this.color, this.cardChild});
+
   final Color color;
-  ReuseableCard({@required this.color});
+  final Widget cardChild;
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
