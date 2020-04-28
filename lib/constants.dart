4f90022c5931af3ba48fc3008afe47f9eb7fec35
Ehmad <justEhmadSaeed@gmail.com>
2020-04-29 01:27:09 +0500

    📦 NEW: Add all Consts into constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = Color(0xFFEB1555);
+
+const labelTextStyle = TextStyle(
+              fontSize: 18.0,
+              color: Color(0xFF808E98),
+            );
