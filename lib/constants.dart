8722dea105d5f1d62988cc913742ff24440790a1
Ehmad <justEhmadSaeed@gmail.com>
2020-04-29 15:14:20 +0500

    📦 NEW: Add kInactiveLabelColor const

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,6 +4,7 @@ const kActiveCardColor = Color(0xFF1D1E33);
 const kInactiveCardColor = Color(0xFF111328);
 const kBottomContainerHeight = 80.0;
 const kBottomContainerColor = Color(0xFFEB1555);
+const kInactiveLabelColor = Color(0xFF808E98);
 
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
