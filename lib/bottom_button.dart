496b2b5494167f4c1a386dcd847bc0cb52e17c59
Ehmad <justEhmadSaeed@gmail.com>
2020-05-02 14:26:52 +0500

    📦 NEW: ADD bottom button widget

---
+++ lib/bottom_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({@required this.bottonTitle, @required this.onTap});
+
+  final Function onTap;
+  final String bottonTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          color: kBottomContainerColor,
+          margin: EdgeInsets.only(top: 10),
+          padding: EdgeInsets.only(bottom: 10),
+          width: double.infinity,
+          height: kBottomContainerHeight,
+          child: Center(
+              child: Text(
+            bottonTitle,
+            style: kLargeButtonStyle,
+          ))),
+    );
+  }
+}
