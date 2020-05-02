5f8d6a04af59ba33cc381438a64d755c23439704
Ehmad <justEhmadSaeed@gmail.com>
2020-05-02 23:55:21 +0500

    🐛 FIX: Add the results from input page to result page

--- lib/Screens/result_page.dart
+++ lib/Screens/result_page.dart
@@ -4,6 +4,14 @@ import '../Components/resusable_card.dart';
 import '../Components/bottom_button.dart';
 
 class ResultPage extends StatelessWidget {
+  ResultPage(
+      {@required this.bmiResult,
+      @required this.interpretation,
+      @required this.resultText});
+
+  final String bmiResult;
+  final String resultText;
+  final String interpretation;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,15 +41,15 @@ class ResultPage extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
                       Text(
-                        'Normal',
+                        resultText.toUpperCase(),
                         style: kResultTextStyle,
                       ),
                       Text(
-                        '18.2',
+                        bmiResult,
                         style: kBMITextStyle,
                       ),
                       Text(
-                        'Your BMI result is quite low, you should eat more!',
+                        interpretation,
                         style: kBodyTextStyle,
                         textAlign: TextAlign.center,
                       ),
